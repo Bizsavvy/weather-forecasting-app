a0bdade37767738875591a2c6c95aa9a7e035e4b
Haye Binjo <binjobiz@gmail.com>
2024-10-15 06:00:09 -0700

    Day 32: Added functions in the backend to filter data.

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input(label="Place:")
 days = st.slider(label="Forecast Days:", min_value=1, max_value=5,
                  help="Select the number of forecasted days.")
 option = st.selectbox(label="Select Data to view:",
-                    options=("Temperature", "Cloud"))
+                      options=("Temperature", "Cloud"))
 
 st.subheader(f"{option} for the next {days} days in {place}")
 
+d, t = get_data(place, days, option)
 
-def get_data(days):
-    dates = ["2023-10-27", "2023-10-28", "2023-10-29"]
-    temperatures = [11, 12, 15]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
-
-
-d, t = get_data(days)
 figure = px.line(x=d, y=t,
                  labels={"x": "Date", "y": "Temperature(C)"})
 st.plotly_chart(figure)
